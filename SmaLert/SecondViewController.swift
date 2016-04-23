737fd257aa6162a49b2f8f7eb5e4e207fb8a174c
Kalissaac <Kian@kians-air.home>
2016-04-22 19:13:06 -0700

    Initial Commit

---
+++ SmaLert/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SmaLert
+//
+//  Created by Kian on 4/22/16.
+//  Copyright © 2016 Kian Sutarwala. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
