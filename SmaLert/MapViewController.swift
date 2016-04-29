6ee0f8a728e265a88256a3ac1bb9dea46ec488fa
Kalissaac <Kian@kians-air.home>
2016-04-28 18:06:43 -0700

    Work on 'Map' tab.

--- SmaLert/MapViewController.swift
+++ SmaLert/MapViewController.swift
@@ -7,27 +7,60 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 import iAd
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, ADBannerViewDelegate {
     
-    var adForVC: UIViewController!
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var adBannerView: ADBannerView!
     
     ;override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-        adForVC.canDisplayBannerAds = true
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if #available(iOS 8.0, *) {
+            self.locationManager.requestWhenInUseAuthorization()
+        } else {
+            // Hope they don't mind their location being used. :P
+        }
+        self.locationManager.startUpdatingLocation()
+        
+        self.canDisplayBannerAds = true
+        self.adBannerView.delegate = self
+        self.adBannerView.hidden = true // Hide until ad loaded.
         
     }
     
-
+    func bannerViewWillLoadAd(banner: ADBannerView!) {
+        NSLog("bannerViewWillLoadAd")
+    }
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        NSLog("bannerViewDidLoadAd")
+        self.adBannerView.hidden = false // Now show banner as ad is loaded.
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    // MARK: - Location Delegate Methods
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated:true)
+        self.locationManager.stopUpdatingLocation()
+    }
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error(s):" + error.localizedDescription)
+    }
 
 }
 
