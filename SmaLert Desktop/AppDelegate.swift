25d5318d735d3644ba9496148d0a8df6f6b2a0c0
Kalissaac <Kian@kians-air.home>
2016-05-13 08:32:52 -0700

    Added desktop app.

---
+++ SmaLert Desktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmaLert Desktop
+//
+//  Created by Kian on 5/13/16.
+//  Copyright © 2016 Kian Sutarwala. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SmaLert Desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SmaLert Desktop
+//
+//  Created by Kian on 5/13/16.
+//  Copyright © 2016 Kian Sutarwala. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
