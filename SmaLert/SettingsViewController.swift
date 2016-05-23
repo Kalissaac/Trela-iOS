d6a3d48778e5edd5bbd0166e2da4b3969654948b
Kalissaac <Kian@kians-air.home>
2016-05-22 17:53:58 -0700

    Removed desktop app.

--- SmaLert/SettingsViewController.swift
+++ SmaLert/SettingsViewController.swift
@@ -2,66 +2,94 @@
 //  SettingsViewController.swift
 //  SmaLert
 //
-//  Created by Kian on 4/10/16.
+//  Created by Kian on 5/22/16.
 //  Copyright © 2016 Kian Sutarwala. All rights reserved.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    // Settings table data.
-    let dataSettingsSec1:[String] = ["Map Type", "Home Location", "Setting 3", "Setting 4", "Setting 5"]
-    let dataSettingsSec2:[String] = ["Setting 6", "Setting 7", "Setting 8", "Setting 9", "Setting 10"]
-    let dataSettingsSec3:[String] = ["Terms and Conditions", "Privacy Policy"]
-    let dataSettingsSec4:[String] = ["Help", "About", "Acknowledgements"]
- 
+class SettingsViewController: UITableViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    // MARK: This is the cell part of the table.
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cellSetting", forIndexPath: indexPath)
-        if indexPath.section == 0 {
-            cell.textLabel?.text = dataSettingsSec1[indexPath.row]
-        } else if indexPath.section == 1 {
-            cell.textLabel?.text = dataSettingsSec2[indexPath.row]
-        } else if indexPath.section == 2 {
-            cell.textLabel?.text = dataSettingsSec3[indexPath.row]
-        } else {
-            cell.textLabel?.text = dataSettingsSec4[indexPath.row]
-        }
-        
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
         return cell
     }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return dataSettingsSec1.count
-        } else if section == 1 {
-            return dataSettingsSec2.count
-        } else if section == 2 {
-            return dataSettingsSec3.count
-        } else {
-            return dataSettingsSec4.count
-        }
-        
-    }
-    
-    
-    // MARK: This is the section part of the table.
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 4
-    }
-    
-    
-}
+    */
 
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
