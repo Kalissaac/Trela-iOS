956dbc0181b575dc66ae0e2106873a9ac60dca36
Kalissaac <Kian@kians-air.home>
2016-06-04 10:37:48 -0700

    Version Beta 2.6 - Added login screen.

--- SmaLert/LoginViewController.swift
+++ SmaLert/LoginViewController.swift
@@ -13,6 +13,34 @@ class LoginViewController: UIViewController {
     @IBAction func noAccount(sender: UIButton) {
         UIApplication.sharedApplication().openURL(NSURL(string: "http://kalissaac.github.io/SmaLert")!)
     }
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginIndicator: UIActivityIndicatorView!
+    @IBAction func didPressLogin(sender: AnyObject) {
+        loginIndicator.startAnimating()
+        loginButton.selected = true
+        // If both the email and password fields match what we are looking for... 
+        if emailField.text == "Kian" && passwordField.text == "Kian" {
+            delay(2, closure: { () -> () in
+            self.loginIndicator.stopAnimating()
+            self.loginButton.selected = false
+            self.performSegueWithIdentifier("loginSegue", sender: nil) })
+        // Otherwise, email or password are incorrect so...
+        } else {
+            delay(2, closure: { () -> () in
+            self.loginIndicator.stopAnimating()
+            self.loginButton.selected = false
+            let alertController = UIAlertController(title: "Oops!", message: "The email adress and/or password you entered were not reconized!", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "Try again", style: .Cancel) { (action) in
+            }
+            alertController.addAction(cancelAction)
+            self.presentViewController(alertController, animated: true) {
+            } })
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
